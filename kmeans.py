957eca22223339afa28fee9160cac609c66a5e40
Abishek <abishek20030324@gmail.com>
2022-11-07 12:44:16 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -4,53 +4,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-def distance (x,y):
-    dist=0
+def distance(x,y):
+    d=0
     for i in range(len(x)):
-        dist+=np.square(x[i]-y[i])
-    return np.sqrt(dist)
-
-def kmeans(data, k):
-    n=len(data[0])
-    new_cluster=old_cluster = [data[random.randint(0, len(data)-1)] for i in range(k)]
+        d+=(x[i]-y[i])**2
+    return np.sqrt(d)
     
+def kmeans(data,k):
+    n=len(data[0])
+    old_centroid = new_centroid = [data[random.randint(0, len(data)-1)] for i in range(k)]
     while True:
         labels=[]
         count=np.zeros(k)
         for i in range(len(data)):
             dist=[]
             for j in range(k):
-                dist.append(distance(data[i],old_cluster[j]))
+                dist.append(distance(data[i],old_centroid[j]))
             temp=dist.index(min(dist))
             labels.append(temp)
             count[temp]+=1
             
         for i in range(k):
-            new_cluster[i]=np.zeros(n)
+            new_centroid[i]=np.zeros(n)
         
         for i in range(len(data)):
-            new_cluster[labels[i]] += data[i]/count[labels[i]]
+            new_centroid[labels[i]] += data[i]/count[labels[i]]
             
-        if np.array_equiv(old_cluster, new_cluster):
+        if np.array_equiv(old_centroid, new_centroid):
             break
         else:
-            old_cluster = list(new_cluster)
+            old_centroid = list(new_centroid)
         
-    return {"labels":labels, "old": old_cluster}
+    return {"labels":labels, "old": old_centroid}
         
 
 iris = datasets.load_iris()
 data = iris.data
 
-inertias = []
-for i in range(1, 11):
+inertias=[]
+for i in range(1,21):
     model = KMeans(n_clusters=i)
-    model.fit(data)
-    inertias.append(kmeans.inertia_)
+    model.fit_transform(data)
+    inertias.append(model.inertia_)
 
-plt.plot(range(1, 11), inertias, marker='o')
-plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
-plt.show()
+plt.plot(range(1,21),inertias,marker='o')
 
-print(kmeans(data,3))
+y_pred=kmeans(data,3)
+print(y_pred)
